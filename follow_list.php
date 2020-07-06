980c09fe542d70303e067c0751fef77a06f31c0a
unknown <kiyana.jalali1999@gmail.com>
2020-07-06 12:47:24 +0430

    follow tamam mahbobiat mond

--- follow_list.php
+++ follow_list.php
@@ -4,23 +4,38 @@ include 'connect.php';
 
 $user_id=$_POST['user_id'];
 
-$sql="SELECT COUNT(*) FROM follow where user1_id=? ";
+$sql="SELECT * FROM follow where user1_id=? ";
 $search= $connect -> prepare($sql);
 $search ->execute([$user_id]);
-$user[0]['following'] = $search->fetchAll(PDO::FETCH_ASSOC)[0]['COUNT(*)'];
+$user1= $search->fetchAll(PDO::FETCH_ASSOC);
 
-$sql="SELECT COUNT(*) FROM follow where user2_id=? ";
+$sql="SELECT * FROM follow where user2_id=? ";
 $search= $connect -> prepare($sql);
 $search ->execute([$user_id]);
-$user[0]['follower'] = $search->fetchAll(PDO::FETCH_ASSOC)[0]['COUNT(*)'];
+$user2 = $search->fetchAll(PDO::FETCH_ASSOC);
 
-for($i=0; $i<sizeof();$i++){
-    
+for($i=0; $i<sizeof($user1);$i++){
+    $sql="SELECT user_name FROM user where user_id=?";
+    $search= $connect -> prepare($sql);
+    $search ->execute([$user1[$i]['user2_id']]);  
+    $following[$i]= $search->fetchAll(PDO::FETCH_ASSOC)[0]['user_name'];
+
+ 
 
 }
 
+for($i=0; $i<sizeof($user2);$i++){
+    $sql="SELECT user_name FROM user where user_id=?";
+    $search= $connect -> prepare($sql);
+    $search ->execute([$user1[$i]['user1_id']]);  
+    $follower[$i]= $search->fetchAll(PDO::FETCH_ASSOC)[0]['user_name'];
 
-$json_res=json_encode($user);
+
+}
+$result['following']=$following;
+$result['follower']=$follower;
+
+$json_res=json_encode($result);
 
 echo $json_res;
 ?>
