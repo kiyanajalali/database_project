1dfb7542fa84e2d56006860cb5b27592cdb70621
mozhgan <mozhepeivnd@gmail.com>
2020-07-16 00:42:09 -0700

    search

--- verify_artist.php
+++ verify_artist.php
@@ -6,12 +6,36 @@ $query = " SELECT *   FROM  request_artist";
 $search= $connect -> prepare($query);
 $search ->execute();
 
+echo"<center>";
+echo"<h1>Join Multiple Tables value</h1>";
+echo"<hr/>";
+echo"<table border='1'>
+<tr>
+<th>user_id </th>
+<th>artist_name </th>
+<th>nationality </th>
+<th>activity_start_date </th>
 
-$request_artist = $search->fetchAll(PDO::FETCH_ASSOC);
+</tr>";
 
-$json_res=json_encode($request_artist);
+while($row = $search->fetch(PDO::FETCH_BOTH) ){
+    echo"<tr>";
+    echo"<td>".$row['user_id']."</td>";
+    echo"<td>".$row['artist_name']."</td>";
+    echo"<td>".$row['nationality']."</td>";
+    echo"<td>".$row['activity_start_date']."</td>";
 
-echo $json_res;
+    echo"</tr>";
+
+}
+echo "</table>";
+echo "</center>";
+
+// $request_artist = $search->fetchAll(PDO::FETCH_ASSOC);
+
+// $json_res=json_encode($request_artist);
+
+// echo $json_res;
     
 
 ?>
